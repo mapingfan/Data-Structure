0ef3d229e5d9f92549ba3ea1fcc872b3c1211dda
马平凡 <mapingfan@gmail.com>
2018-03-30 11:45:49 +0800

    实现希尔排序的另一种增量序列代码

--- src/SortAlgorithm/ShellSort/ShellSort.java
+++ src/SortAlgorithm/ShellSort/ShellSort.java
@@ -2,6 +2,8 @@ package SortAlgorithm.ShellSort;
 
 import java.util.Arrays;
 
+import static java.lang.Math.pow;
+
 public class ShellSort {
 
     private static void shellSort(int[] arr) {
@@ -24,12 +26,11 @@ public class ShellSort {
     }
 
     private static void shellSortV2(int[] arr) {
-        int len = arr.length;
-        int j;
+        int len = arr.length, j;
         for (int gap = len / 2; gap > 0; gap /= 2) {
-            for (int i = gap; i < len; i++) {
+            for (int i = gap; i < len; i++) { //类比插排从第一个元素开始,希尔从第gap个开始.
                 int temp = arr[i];
-                for (j = i; j >= gap && temp < arr[j - gap]; j -=gap) {
+                for (j = i; j >= gap && temp < arr[j - gap]; j -= gap) {
                     arr[j] = arr[j - gap];
                 }
                 arr[j] = temp;
@@ -37,11 +38,34 @@ public class ShellSort {
         }
     }
 
+    /*
+    换一个增量序列的实现,采用塞得克维奇的序列实现下.
+     */
+    private static void shellSortV3(int[] arr) {
+        int i = 0, j, k;
+        while (arr.length > (9 * (int) Math.pow(4, i) - 9 * (int) pow(2, i) + 1)) {
+            i++;
+        }
+        int index = i - 1;
+        int gap = 9 * (int) Math.pow(4, index) - 9 * (int) pow(2, index) + 1;
+        while (gap > 0 && index >= 0) {
+            for (j = gap; j < arr.length; j++) {
+                int temp = arr[j];
+                for (k = j; k >= gap && temp < arr[k - gap]; k -= gap) {
+                    arr[k] = arr[k - gap];
+                }
+                arr[k] = temp;
+            }
+            index--;
+            gap = 9 * (int) Math.pow(4, index) - 9 * (int) pow(2, index) + 1; //gap最后一定为1
+        }
+    }
+
 
     public static void main(String[] args) {
         //int[] arr = {49, 38, 65, 97, 26, 13, 27, 49, 55, 4};
         int[] arr = {3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48};
-        shellSortV2(arr);
+        shellSortV3(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
