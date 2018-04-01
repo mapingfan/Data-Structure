2d614368051fbda44606c608a6c20ed80464b65f
马平凡 <mapingfan@gmail.com>
2018-04-01 15:40:04 +0800

    提交代码

--- src/SortAlgorithm/RadixSort/RadixSort.java
+++ src/SortAlgorithm/RadixSort/RadixSort.java
@@ -4,14 +4,15 @@ import java.util.Arrays;
 import java.util.LinkedList;
 
 public class RadixSort {
+//   模拟0-9的队列.
     static LinkedList<Integer>[] lists = new LinkedList[10];
-
+//   队列初始化操作
     static {
         for (int i = 0; i < lists.length; i++) {
             lists[i] = new LinkedList<>();
         }
     }
-
+//获取最大元素,通过这个来获取最大的位数.
     private static int findMaxElement(int[] arr) {
         int max = arr[0];
         for (int i = 1; i < arr.length; i++) {
@@ -23,7 +24,7 @@ public class RadixSort {
         }
         return max;
     }
-
+//获取最大位数,版本一.
     private static int getDigits(int num) {
         int count = 0;
         if (num <= 9) {
@@ -37,6 +38,10 @@ public class RadixSort {
         return count;
     }
 
+    /**
+     * @param num 获取num的位数,比如123,是三位.
+     * @return    返回最大位数.
+     */
     private static int getDigitsV2(int num) {
         if (num < 10) {
             return 1;
@@ -47,8 +52,7 @@ public class RadixSort {
 
     /**
      * @param index 要获取第index位
-     * @param num   待处理的数字.
-     *              比如123,获取第2位,返回2.
+     * @param num   待处理的数字. 比如123,获取第2位,返回2.
      */
     private static int getDigitByIndex(int index, int num) {
         int digits = getDigitsV2(num);
@@ -69,11 +73,10 @@ public class RadixSort {
         int index = 0;
         for (int i = 0; i < lists.length; i++) {
             for (int i1 = 0; i1 < lists[i].size(); i1++) {
-                arr[index++] = lists[i].get(i1);
+                arr[index++] = lists[i].get(i1); //这个地方千万不能用poll出队列,会导致循环异常.
             }
             lists[i].clear();
         }
-        System.out.println(Arrays.toString(arr));
     }
 
     /**
@@ -90,7 +93,7 @@ public class RadixSort {
         }
 
     }
-
+//主方法
     private static void radixSort(int[] arr) {
         int maxNum = findMaxElement(arr);
         int maxDigits = getDigitsV2(maxNum);
