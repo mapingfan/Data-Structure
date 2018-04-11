7214e8e7315a0e80ecbcf57be36ae3cbf956d3fe
马平凡 <mapingfan@gmail.com>
2018-04-11 14:59:46 +0800

    补充下快速幂的实现.其实就是二分法.

--- src/Recursion/Power.java
+++ src/Recursion/Power.java
@@ -7,41 +7,58 @@ import java.awt.*;
  */
 public class Power {
     public static void main(String[] args) {
-        System.out.println(Power.power(2,5));
-        System.out.println(Power.power_v2(2,5));
-        System.out.println(Math.pow(2,5));
+        System.out.println(casse(2, 5, 7));
     }
 
     public static int power(int x, int n) { //to simplify problem, assume x > 0;
-        /*if (n == 0) {
-            return 1;
-        } else {
-            int tmp = power(x, n / 2);
-            if (n % 2 == 0) {
-                return tmp*tmp;
-            } else {
-                return tmp*tmp*x;
-            }
-        }*/
         int result;
         if (n == 0) {
             result = 1;
         } else {
-            result = power(x,n/2);
-            result = result*result;
+            result = power(x, n / 2);
+            result = result * result;
             if (n % 2 == 1) {
-                result = result*x;
+                result = result * x;
             }
         }
         return result;
+    }
+
+
+    public static long powerV3(int x, int n) {
+        long result = 1;
+        while (n != 0) {
+            if ((n & 1) != 0) {
+                result = result * x;
+            }
+            n >>= 1;
+            x = x * x;
+        }
+        return result;
+    }
+
+
+    /**
+     * a^b mod c = (a mod c)^b mod c  快速取模算法.
+     */
+
+    public static long casse(int a, int b, int c) {
+        if (a == 0) {
+            return 0;
+        }
+        if (a > 0 && a < c) {
+            return ((powerV3(a, b)) % c);
+        }
+        return casse(a % c, b, c);
     }
 
     public static int power_v2(int x, int n) {
         int result = 1;
         for (int i = 1; i <= n; i++) {
-            result = result*x;
+            result = result * x;
 
         }
         return result;
     }
+
 }
