61616ee4d5a875040b55967e1054708b61557135
马平凡 <mapingfan@gmail.com>
2018-04-11 20:13:09 +0800

    最大1子方阵.暂时不会做,先把答案写下来.

---
+++ src/DynamicProgramming/MaxSubSquaree/MaxSubSquare.java
@@ -0,0 +1,36 @@
+package DynamicProgramming.MaxSubSquaree;
+
+/**
+ * 在一个0,1构成的矩阵中,找一个最大子方阵,全由1构成.
+ * <p>
+ * 0 1 1 0 1
+ * 1 1 0 1 0
+ * 0 1 1 1 0
+ * 1 1 1 1 0
+ * 1 1 1 1 1
+ * 0 0 0 0 0
+ * <p>
+ * 最大方阵肉眼可见,但是如何用代码求呢?
+ * <p>
+ * 这个地方先把答案抄写下来.
+ * 1) 原矩阵为A[m][n],基于这个矩阵构造一个L[m][n],
+ * 2) 把L[m][n]的第一行第一设置同A[m][n]一样.
+ * 3) 对于L中的其他元素,按照下面公司计算:
+ * if(A[i][j]!=0){
+ * L[i][j] = min(L[i-1][j],L[i-1][j-1],L[i][j-1])+1;
+ * } else{
+ * L[i][j] = 0;
+ * }
+ * 4)找出L[m][n]中的最大值,并且记录最大值的两个下标.
+ * 5)根据找出的最大值,和连个下标.去A中输出子阵.
+ * for(int i = max_i; i> max_i - max_L; i--){
+ *      for(int j = max_j; j>max_j - max_L; j--){
+ *          System.out.print(A[i][j]+" ");
+ *      }
+ *      System.out.println();
+ * }
+ *
+ * 时空复杂度都是O(mn).
+ */
+public class MaxSubSquare {
+}
