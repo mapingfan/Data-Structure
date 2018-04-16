9e408258b5cf870fa92ed992379c5f93d12b00d2
马平凡 <mapingfan@gmail.com>
2018-04-16 21:25:00 +0800

    补充一些技巧

--- src/Others/Others.java
+++ src/Others/Others.java
@@ -1,4 +1,43 @@
 package Others;
 
+/**
+ * 补充一些小技巧,主要是移位操作.
+ */
 public class Others {
+
+    //检测第K位是否为1.
+    private static boolean kthBit(int n, int k) {
+        return (n & (1 << k - 1)) == (int) (Math.pow(2, (k - 1)));
+    }
+
+    //将k位置为1.
+    private static int setKthBit(int n, int k) {
+        return n = n | (1 << k - 1);
+    }
+
+    //k位置为0.
+    private static int setKthZero(int n, int k) {
+        return n = n & (~(1 << k - 1));
+    }
+
+    //切换值为1的最右位:
+    private static int flip(int n) {
+        return n & n - 1;
+    }
+
+    //统计1的个数.
+    private static int count(int n) {
+        int count = 0;
+        while (n != 0) {
+            count += n & 1;
+            n = n >> 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(count(10));
+        System.out.println(kthBit(10, 1));
+
+    }
 }
